64e1ecd259cf6927810e207f480151292fb1f670
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-05-20 16:20:17 -0300

    config PHPMAILER

--- php/form-process.php
+++ php/form-process.php
@@ -73,7 +73,7 @@ $body .= $message;
 $body .= "\n";
 
 try {
-      $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+      //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
       $mail->CharSet = 'UTF-8';
       $mail->isSMTP();
       $mail->Host = 'smtp.gmail.com';
@@ -88,7 +88,7 @@ try {
       $mail->Subject = " ".$subject;
       $mail->Body = " ".$body;
 
-      if ($mail->send()){
+      if  ($mail->send()){
         echo 'Sucesso';
      }else{
          echo 'email n enviado';
