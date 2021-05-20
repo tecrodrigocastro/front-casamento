d18d3fe2bf3d1c335afef4858f82dc59a020b7c9
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-05-20 15:26:32 -0300

    Add PHPMailer

--- php/form-process.php
+++ php/form-process.php
@@ -1,5 +1,16 @@
 <?php
 
+require_once('../src/PHPMailer.php');
+require_once('../src/SMTP.php');
+require_once('../src/Exception.php');
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
 $errorMSG = "";
 
 // NAME
@@ -61,13 +72,45 @@ $body .= "Mensagem: ";
 $body .= $message;
 $body .= "\n";
 
+try {
+      $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+      $mail->CharSet = 'UTF-8';
+      $mail->isSMTP();
+      $mail->Host = 'smtp.gmail.com';
+      $mail->SMTPAuth = true;
+      $mail->Username = 'tec.rodrigocastro@gmail.com';
+      $mail->Password = 'codeofsucess';
+      $mail->Port = 587;
+      
+      $mail-> setFrom($email);
+      $email->setAddress($emailTo);
+
+      $mail->Subject = $subject;
+      $mail->Body = $body;
+
+      if ($mail->send() && $errorMSG == ""){
+        echo "Sucesso";
+     }else{
+         if($errorMSG == ""){
+             echo "Algo deu errado :(";
+         } else {
+             echo $errorMSG;
+         }
+     }
+
+    
+      
+} catch (Exception $e) {
+    echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+}
+
 
 // send email
-$success = mail($emailTo, $subject, $body, $email);
+//$success = mail($emailTo, $subject, $body, $email);
 
 
 // redirect to success page
-if ($success == false && $errorMSG == ""){
+/*if ($success && $errorMSG == ""){
    echo "Sucesso";
 }else{
     if($errorMSG == ""){
@@ -76,5 +119,6 @@ if ($success == false && $errorMSG == ""){
         echo $errorMSG;
     }
 }
+*/
 
 ?>
