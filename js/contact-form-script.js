0634765fd27e5dcccffc1dd9059a5222aedc45d3
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-05-20 12:45:54 -0300

    modifier var contact-form

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -24,7 +24,7 @@ function submitForm(){
     $.ajax({
         type: "POST",
         url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&guest" + guest + "&event" + event + "&msg_subject=" + msg_subject + "&message=" + message,
+        data: "name=" + name + "&email=" + email + "&guest=" + guest + "&event=" + event + "&msg_subject=" + msg_subject + "&message=" + message,
         success : function(text){
             if (text == "success"){
                 formSuccess();
