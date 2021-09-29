dbfcd5f6988bd0ef4033a0a92c016c37a7bc228e
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-09-29 19:34:03 -0300

    LISTA DE PRESENTES

--- index.php
+++ index.php
@@ -1,5 +1,435 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
 
-include_once("index.html");
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-?>
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<!-- Site Metas -->
+	<title>Modelo</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Site Icons -->
+	<link rel="shortcut icon" href="" type="image/x-icon">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Pogo Slider CSS -->
+	<link rel="stylesheet" href="css/pogo-slider.min.css">
+	<!-- Site CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Responsive CSS -->
+	<link rel="stylesheet" href="css/responsive.css">
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/custom.css">
+
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+
+	<!-- LOADER -->
+	<div id="preloader">
+		<div class="preloader pulse">
+			<i class="fa fa-heartbeat" aria-hidden="true"></i>
+		</div>
+	</div><!-- end loader -->
+	<!-- END LOADER -->
+
+	<!-- Start header -->
+	<header class="top-header">
+		<nav class="navbar header-nav navbar-expand-lg">
+			<div class="container">
+				<a class="navbar-brand" href=""><img src="images/logo2.jpg" alt="image"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd"
+					aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="navbar-wd">
+					<ul class="navbar-nav">
+						<li><a class="nav-link active" href="#home">Início</a></li>
+						<li><a class="nav-link" href="#about">Sobre nós</a></li>
+						<li><a class="nav-link" href="#story">História</a></li>
+						<li><a class="nav-link" href="#gallery">Galeria</a></li>
+						<li><a class="nav-link" href="list.php">Lista de Presentes</a></li>
+						<li><a class="nav-link" href="#contact">Confirmação</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- End header -->
+
+	<!-- Start Banner -->
+	<div class="ulockd-home-slider">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="pogoSlider" id="js-main-slider">
+					<div class="pogoSlider-slide" data-transition="zipReveal" data-duration="1500"
+						style="background-image:url(images/slider-01.jpg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>Bruna & Daniel</h1>
+								<h2>Nós vamos nos casar<h3>
+										<p>Reserve a data <strong> 20 Out 2021</strong></p>
+										<a href="#contact" class="btn ">Confirmar</a>
+							</div>
+						</div>
+					</div>
+					<div class="pogoSlider-slide" data-transition="blocksReveal" data-duration="1500"
+						style="background-image:url(images/slider-02.jpg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>Bruna & Daniel</h1>
+								<h2>Nós vamos nos casar<h3>
+										<p>Reserve a data <strong> 20 Out 2021</strong></p>
+										<a href="#contact" class="btn ">Confirmar</a>
+							</div>
+						</div>
+					</div>
+					<div class="pogoSlider-slide" data-transition="shrinkReveal" data-duration="2000"
+						style="background-image:url(images/slider-03.jpg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>Bruna & Daniel</h1>
+								<h2>Nós vamos nos casar<h3>
+										<p>Reserve a data <strong> 20 Out 2021</strong></p>
+										<a href="#contact" class="btn ">Confirmar</a>
+							</div>
+						</div>
+
+					</div>
+				</div><!-- .pogoSlider -->
+			</div>
+		</div>
+	</div>
+	<!-- End Banner -->
+
+	<!-- Start About us -->
+	<div id="about" class="about-box">
+		<div class="about-a1">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="title-box">
+							<h2>Bruna <span>&</span> Daniel</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12">
+						<div class="row align-items-center about-main-info">
+							<div class="col-lg-8 col-md-6 col-sm-12">
+								<h2> Sobre <span>Bruna</span></h2>
+								<p>Texto sobre, Texto sobre, Texto sobre, Texto sobre, Texto sobre, .
+								</p>
+							</div>
+							<div class="col-lg-4 col-md-6 col-sm-12">
+								<div class="about-img">
+									<img class="img-fluid rounded" src="images/about-img-01.jpg" alt="" />
+								</div>
+							</div>
+						</div>
+						<div class="row align-items-center about-main-info">
+							<div class="col-lg-4 col-md-6 col-sm-12">
+								<div class="about-img">
+									<img class="img-fluid rounded" src="images/about-img-02.jpg" alt="" />
+								</div>
+							</div>
+							<div class="col-lg-8 col-md-6 col-sm-12">
+								<h2> Sobre <span>Daniel</span></h2>
+								<p>Texto sobre, Texto sobre, Texto sobre, Texto sobre, Texto sobre, .
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About us -->
+
+	<!-- Start Story -->
+	<div id="story" class="story-box main-timeline-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Nossa História</h2>
+						<p>Linha do tempo. </p>
+					</div>
+				</div>
+			</div>
+			<div class="row timeline-element separline">
+				<div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+					<div class="time-line-date-content">
+						<p class="mbr-timeline-date mbr-fonts-style display-font">
+							1 Março de 2021
+						</p>
+					</div>
+				</div>
+				<span class="iconBackground"></span>
+				<div class="col-xs-12 col-md-6 align-left">
+					<div class="timeline-text-content">
+						<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-font">Primeiro Encontro</h4>
+						<p class="mbr-timeline-text mbr-fonts-style display-7">
+							Nós nos conhecemos no casamento de nossos amigos próximos e imediatamente encontramos uma
+							linguagem comum, então, um ano depois, nosso primeiro encontro aconteceu.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row timeline-element reverse separline">
+				<div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+					<div class="time-line-date-content">
+						<p class="mbr-timeline-date mbr-fonts-style display-font">
+							2 Abril de 2021
+						</p>
+					</div>
+				</div>
+				<span class="iconBackground"></span>
+				<div class="col-xs-12 col-md-6 align-right">
+					<div class="timeline-text-content">
+						<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-font">Inicio do namoro</h4>
+						<p class="mbr-timeline-text mbr-fonts-style display-7">
+							Texto ...
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row timeline-element separline">
+				<div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+					<div class="time-line-date-content">
+						<p class="mbr-timeline-date mbr-fonts-style display-font">
+							1 Maio de 2021
+						</p>
+					</div>
+				</div>
+				<span class="iconBackground"></span>
+				<div class="col-xs-12 col-md-6 align-left">
+					<div class="timeline-text-content">
+						<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-font">Noivado</h4>
+						<p class="mbr-timeline-text mbr-fonts-style display-7">
+							Noivamos...
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row timeline-element reverse separline">
+				<div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+					<div class="time-line-date-content">
+						<p class="mbr-timeline-date mbr-fonts-style display-font">
+							20 Outubro de 2021
+						</p>
+					</div>
+				</div>
+				<span class="iconBackground"></span>
+				<div class="col-xs-12 col-md-6 align-right">
+					<div class="timeline-text-content">
+						<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-font">Casamento</h4>
+						<p class="mbr-timeline-text mbr-fonts-style display-7">
+							Casamos...
+						</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<!-- End Story -->
+
+	<!-- Start Family 
+	
+	 End Family -->
+
+	<!-- Start Gallery -->
+	<div id="gallery" class="gallery-box">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Galeria</h2>
+						<p>Alguns dos nossos melhores momentos. </p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<ul class="popup-gallery clearfix">
+					<li>
+						<a href="images/gallery-01.jpg">
+							<img class="img-fluid" src="images/gallery-01.jpg" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/gallery-02.jpg">
+							<img class="img-fluid" src="images/gallery-02.jpg" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/gallery-03.jpg">
+							<img class="img-fluid" src="images/gallery-03.jpg" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/gallery-04.jpg">
+							<img class="img-fluid" src="images/gallery-04.jpg" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/gallery-05.jpg">
+							<img class="img-fluid" src="images/gallery-05.jpg" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/gallery-06.jpg">
+							<img class="img-fluid" src="images/gallery-06.jpg" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/gallery-07.jpg">
+							<img class="img-fluid" src="images/gallery-07.jpg" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/gallery-08.jpg">
+							<img class="img-fluid" src="images/gallery-08.jpg" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- End Gallery -->
+
+	<!-- Start Events -->
+	
+	<!-- End Events -->
+
+	<!-- Start Contact -->
+	<div id="contact" class="contact-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Confirme sua Presença</h2>
+						<p>Informações Adicionais... </p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12 col-sm-12 col-xs-12">
+					<div class="contact-block">
+						<form id="contactForm">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<input type="text" class="form-control" id="name" name="name"
+											placeholder="Seu Nome" required data-error="Por favor, insira seu nome">
+										<div class="help-block with-errors"></div>
+									</div>
+								</div>
+								<br>
+								<div class="col-md-6">
+									<div class="form-group">
+										<input type="text" placeholder="Seu E-mail" id="email" class="form-control"
+											name="email" required data-error="Por favor, insira seu email">
+										<div class="help-block with-errors"></div>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="form-group">
+										<select class="custom-select d-block form-control" id="guest" name="guest" required
+											data-error="Selecione um item da lista.">
+											<option disabled selected>Numero de convidados</option>
+											<option value="1">1</option>
+											<option value="2">2</option>
+											<option value="3">3</option>
+											<option value="4">4</option>
+											<option value="5">5</option>
+										</select>
+										<div class="help-block with-errors"></div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<select class="custom-select d-block form-control" id="event" name="event" required
+											data-error="Selecione um item da lista.">
+											<option disabled selected>Você irá ao evento?</option>
+											<option value="Sim">Sim</option>
+											<option value="Não">Não</option>
+										</select>
+
+										<div class="help-block with-errors"></div>
+									</div>
+								</div>
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="form-control" id="message" name="message" placeholder="Deixe uma mensagem"
+											rows="8" data-error="Escreva sua mensagem" required></textarea>
+										<div class="help-block with-errors"></div>
+									</div>
+									<div class="submit-button text-center">
+										<button class="btn btn-common" id="submit" name="submit" type="submit">Enviar
+											mensagem</button>
+										<div id="msgSubmit" class="h3 text-center hidden"></div>
+										<div class="clearfix"></div>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact -->
+
+	<!-- Start Footer -->
+	<footer class="footer-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="footer-company-name">Todos os direitos reservados. &copy; 2021 Feito por : <a
+							href="https://www.instagram.com/redrodrigoc/">Rodrigo Castro</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer -->
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<!-- ALL PLUGINS -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/jquery.pogo-slider.min.js"></script>
+	<script src="js/slider-index.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/form-validator.min.js"></script>
+	<script src="js/contact-form-script.js"></script>
+	<script src="js/custom.js"></script>
+</body>
+
+</html>
---
+++ list.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<!-- Site Metas -->
+	<title>Modelo</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Site Icons -->
+	<link rel="shortcut icon" href="" type="image/x-icon">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Pogo Slider CSS -->
+	<link rel="stylesheet" href="css/pogo-slider.min.css">
+	<!-- Site CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Responsive CSS -->
+	<link rel="stylesheet" href="css/responsive.css">
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/custom.css">
+
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+
+	<!-- LOADER -->
+	<div id="preloader">
+		<div class="preloader pulse">
+			<i class="fa fa-heartbeat" aria-hidden="true"></i>
+		</div>
+	</div><!-- end loader -->
+	<!-- END LOADER -->
+
+	<!-- Start header -->
+	<header class="top-header">
+		<nav class="navbar header-nav navbar-expand-lg">
+			<div class="container">
+				<a class="navbar-brand" href=""><img src="images/logo2.jpg" alt="image"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd"
+					aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="navbar-wd">
+					<ul class="navbar-nav">
+						<li><a class="nav-link active" href="index.php#home">Início</a></li>
+						<li><a class="nav-link" href="index.php#about">Sobre nós</a></li>
+						<li><a class="nav-link" href="index.php#story">História</a></li>
+						<li><a class="nav-link" href="index.php#gallery">Galeria</a></li>
+						<li><a class="nav-link" href="list.php">Lista de Presentes</a></li>
+						<li><a class="nav-link" href="index.php#contact">Confirmação</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- End header -->
+
+	<!-- Start Banner -->
+	<div class="ulockd-home-slider">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="pogoSlider" id="js-main-slider">
+					<div class="pogoSlider-slide" data-transition="zipReveal" data-duration="1500"
+						style="background-image:url(images/slider-01.jpg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>Bruna & Daniel</h1>
+								<h2>Nós vamos nos casar<h3>
+										<p>Reserve a data <strong> 20 Out 2021</strong></p>
+										<a href="#contact" class="btn ">Confirmar</a>
+							</div>
+						</div>
+					</div>
+					<div class="pogoSlider-slide" data-transition="blocksReveal" data-duration="1500"
+						style="background-image:url(images/slider-02.jpg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>Bruna & Daniel</h1>
+								<h2>Nós vamos nos casar<h3>
+										<p>Reserve a data <strong> 20 Out 2021</strong></p>
+										<a href="#contact" class="btn ">Confirmar</a>
+							</div>
+						</div>
+					</div>
+					<div class="pogoSlider-slide" data-transition="shrinkReveal" data-duration="2000"
+						style="background-image:url(images/slider-03.jpg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>Bruna & Daniel</h1>
+								<h2>Nós vamos nos casar<h3>
+										<p>Reserve a data <strong> 20 Out 2021</strong></p>
+										<a href="#contact" class="btn ">Confirmar</a>
+							</div>
+						</div>
+
+					</div>
+				</div><!-- .pogoSlider -->
+			</div>
+		</div>
+	</div>
+	<!-- End Banner -->
+
+	<!-- Start About us -->
+
+	<!-- End About us -->
+
+	<!-- Start Story -->
+
+	<!-- End Story -->
+
+	<!-- Start Family 
+	
+	 End Family -->
+
+	<!-- Start Gallery -->
+	
+	<!-- End Gallery -->
+
+	<!-- Start Events -->
+	<div id="events" class="events-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Nossa lista de presentes</h2>
+						<p>Compre nossos presentes através do aplicativo <strong>Picpay</strong> e realize nossos sonhos</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="event-inner">
+						<div class="event-img">
+							<img class="img-fluid" src="images/presentes/geladeira.jpg" alt="" />
+						</div>
+						<h2>Geladeira Frost Free Duplex</h2>
+						<p>R$ 3.200,00 </p>
+						<a href="https://app.picpay.com/user/redrodrigoc/3200,00">Comprar ></a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="event-inner">
+						<div class="event-img">
+							<img class="img-fluid" src="images/presentes/fogao2.jpg" alt="" />
+						</div>
+						<h2>Fogão de Embutir 5 Bocas</h2>
+						<p>R$ 1.500,00 </p>
+						<a href="https://app.picpay.com/user/redrodrigoc/1500,00">Comprar ></a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="event-inner">
+						<div class="event-img">
+							<img class="img-fluid" src="images/presentes/tv.jpg" alt="" />
+						</div>
+						<h2>Televisão 50”  </h2>
+						<p>R$ 2.300,00 </p>
+						<a href="https://app.picpay.com/user/redrodrigoc/2300,00">Comprar ></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Events -->
+
+	<!-- Start Contact -->
+	
+	<!-- End Contact -->
+
+	<!-- Start Footer -->
+	<footer class="footer-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="footer-company-name">Todos os direitos reservados. &copy; 2021 Feito por : <a
+							href="https://www.instagram.com/redrodrigoc/">Rodrigo Castro</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer -->
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<!-- ALL PLUGINS -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/jquery.pogo-slider.min.js"></script>
+	<script src="js/slider-index.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/form-validator.min.js"></script>
+	<script src="js/contact-form-script.js"></script>
+	<script src="js/custom.js"></script>
+</body>
+
+</html>
