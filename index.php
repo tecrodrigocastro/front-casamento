3872d1dadb25ea56b9020c274a44b0dcd923acba
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-10-16 11:31:38 -0300

    logo

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 	<!-- Site Metas -->
-	<title>Modelo</title>
+	<title>Bruna&Daniel</title>
 	<meta name="keywords" content="">
 	<meta name="description" content="">
 	<meta name="author" content="">
@@ -51,7 +51,7 @@
 	<header class="top-header">
 		<nav class="navbar header-nav navbar-expand-lg">
 			<div class="container">
-				<a class="navbar-brand" href=""><img src="images/logo2.jpg" alt="image"></a>
+				<a class="navbar-brand" href=""><img src="images/11.jpg" alt="image"></a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd"
 					aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
 					<span></span>
--- list.php
+++ list.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 	<!-- Site Metas -->
-	<title>Modelo</title>
+	<title>Bruna&Daniel</title>
 	<meta name="keywords" content="">
 	<meta name="description" content="">
 	<meta name="author" content="">
