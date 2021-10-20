0c453f0303c46178f58284c1e743aa9c892cc995
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-10-20 18:57:19 -0300

    remove sanduicheira list

--- list.php
+++ list.php
@@ -260,7 +260,7 @@
 						</div>
 						<h2>Sanduicheira</h2>
 						<p>R$ 100,00 </p>
-						<a href="https://app.picpay.com/user/dpls/100,00">Comprar ></a>
+						<h2><strong>Esse presente já foi comprado</strong></h2>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6 col-sm-12">
